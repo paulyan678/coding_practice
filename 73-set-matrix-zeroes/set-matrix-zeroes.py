ecd96e5df71d21640415e97586da66f830669ba8
paulyan678 <118022197+paulyan678@users.noreply.github.com>
2024-03-05 10:58:20 -0500

    Time: 105 ms (45.43%) | Memory: 17.5 MB (45.55%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -5,17 +5,20 @@ class Solution:
         """
         m, n = len(matrix), len(matrix[0])
         r, c = set(), set()
+
+        # Identifying rows and columns to be zeroed
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 0:
                     r.add(i)
                     c.add(j)
         
+        # Setting the identified columns to zero
         for j in c:
             for i in range(m):
                 matrix[i][j] = 0
         
+        # Setting the identified rows to zero
         for i in r:
             for j in range(n):
                 matrix[i][j] = 0
-        
